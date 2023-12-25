283e20d6b0e6aeb133fc87c3f89ec0225dfbb8ba
KOBCHUK <irravadi@gmail.com>
2023-12-25 22:50:28 +0200

    ex_1_done

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 try:
+    x = 4
+    y = 4
+    i = j = 0
+    while i < x:
+        j = 0
+        while j < y:
+            if i <= j:
+                print('*  ', end='')
+            else:
+                print('   ', end='')
+            j += 1
+        print()
+        i += 1
 
 except Exception as e:
     print(e)
