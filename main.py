e6f0566620d2aea990600c8533677d49a4185073
KOBCHUK <irravadi@gmail.com>
2023-12-25 22:13:06 +0200

    ex_4_done

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 try:
 
+    n = int(input("Enter the height of the triangle: "))
+
+    i = 0
+    while i <= n:
+        print(" " * (n - i), "*" * (2 * i + 1))
+        i += 1
 except Exception as e:
     print(e)
     print('Error importing modules')
