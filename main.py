a72599f86741f2f8a1a41dc1644f24c7e776130c
KOBCHUK <irravadi@gmail.com>
2023-12-26 14:19:15 +0200

    ex_8_done

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 try:
-
+    i = 0
+    while i < 5:
+        j = 0
+        while j < 5:
+            if (i == 0 and j == 4) or (i == 1 and j in [3, 4]) or (i == 2 and j in [2, 3, 4]) or (
+                    i == 3 and j in [3, 4]) or (i == 4 and j == 4):
+                print("* ", end=" ")
+            else:
+                print("  ", end=" ")
+            j += 1
+        print()
+        i += 1
 except Exception as e:
     print(e)
     print('Error importing modules')
