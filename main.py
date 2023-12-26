bcd8d9d8f9463a34a83fdf2498cebf2a98aba430
KOBCHUK <irravadi@gmail.com>
2023-12-26 19:28:31 +0200

    ex_10_done

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 try:
-
+    i = 0
+    while i < 4:
+        j = 0
+        while j < 4:
+            if i + j >= 3:
+                print('*  ', end='')
+            else:
+                print('   ', end='')
+            j += 1
+        print()
+        i += 1
 except Exception as e:
     print(e)
     print('Error importing modules')
