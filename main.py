7567de762d34351d118b239a09860a482e8e843a
KOBCHUK <irravadi@gmail.com>
2023-12-26 19:33:49 +0200

    ex_3_done

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ try:
     n = int(input("Enter the height of the triangle: "))
     i = 1
     while i <= n:
-        print(" " * (i - 1), "*" * (2 * (n - i) - 1))
+        print("  " * (i - 1), "* " * (2 * (n - i) - 1))
         i += 1
 except Exception as e:
     print(e)
