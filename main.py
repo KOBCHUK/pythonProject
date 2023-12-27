97a8c514148b0410ddb7f7049c6fe97bcba2ff0b
KOBCHUK <irravadi@gmail.com>
2023-12-27 22:14:35 +0200

    ex_6_done_2

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 try:
+    n = int(input("Enter number: "))
+    if n < 0:
+        raise Exception('Number cannot be negative')
+    if n % 2 == 0:
+        raise Exception('Number cannot be even')
     i = 0
-    while i < 5:
+    while i < n:
         j = 0
-        while j < 5:
-            if (i == 0 and j == 1) or (i == 0 and j == 2) or (i == 1 and j == 2) or (i == 3 and j == 2) or (
-                    i == 4 and j == 1) or (i == 4 and j == 2):
-                print("  ", end=" ")
-            elif (i == 0 and j == 3) or (i == 4 and j == 3):
-                print("  ", end=" ")
+        while j < n:
+            if (i <= j and i + j >= n - 1) or (i >= j and i + j <= n - 1):
+                print('* ', end=' ')
             else:
-                print("* ", end=" ")
+                print('  ', end=' ')
             j += 1
         print()
         i += 1
