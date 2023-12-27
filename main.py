451389c2c797800c5e3fc028b83dc31e2539f1ca
KOBCHUK <irravadi@gmail.com>
2023-12-27 22:04:07 +0200

    ex_7_done_2

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 try:
+    n = int(input("Enter numder: "))
+    if n <= 0:
+        raise Exception('Number must be greater than 0')
+    if n % 2 == 0:
+        raise Exception('Number must be odd')
     i = 0
-    while i < 5:
+    while i < n:
         j = 0
-        while j <= i:
-            if (i == 0 and j == 0) or (i == 1 and j <= 1) or (i == 2 and j <= 2) or (i == 3 and j <= 1) or (
-                    i == 4 and j == 0):
-                print("*", end="\t")
+        while j < n:
+            if i >= j and i + j <= n - 1:
+                print('*', end=' ')
             else:
-                print("\t", end="\t")
+                print(' ', end=' ')
             j += 1
         print()
         i += 1
