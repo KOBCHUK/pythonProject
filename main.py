89e47ca4a872379a0a6d6ce68691bce8109f5ff5
KOBCHUK <irravadi@gmail.com>
2023-12-27 22:03:04 +0200

    ex_12_done_3

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 try:
-        n = int(input("Enter number: "))
-        if n < 0:
-            raise Exception('Number must be positive')
-        if n % 2 == 0:
-            raise Exception('Number must be odd')
-        i = 0
-        while i < n:
-            j = 0
-            while j < n:
-                if i <= j and i + j >= n - 1:
-                    print('*', end=' ')
-                else:
-                    print(' ', end=' ')
-                j += 1
-            print()
-            i += 1
-
-
+    n = int(input("Enter numder: "))
+    if n <= 0:
+        raise Exception('Number must be greater than 0')
+    if n % 2 == 0:
+        raise Exception('Number must be odd')
+    i = 0
+    while i < n:
+        j = 0
+        while j < n:
+            if i >= j and i + j <= n - 1:
+                print('*', end=' ')
+            else:
+                print(' ', end=' ')
+            j += 1
+        print()
+        i += 1
 except Exception as e:
     print(e)
     print('Error importing modules')
