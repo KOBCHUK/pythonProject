a30973dc034c644e640ebddc92fb1134e1fc2675
KOBCHUK <irravadi@gmail.com>
2023-12-28 21:25:40 +0200

    ex_11_done_2

--- main.py
+++ main.py
@@ -1,59 +1,19 @@
 try:
-    number_task = int(input("Enter the task: "))
+    number_task = int(input("Enter the task 1-10: "))
     if number_task == 1:
         print("Ex_1:")
-        row = 0
-        while row < n:
-            col = 0
-            while col < n:
-                if row <= col and row + col <= n - 1:
-                    print('*', end=' ')
-                else:
-                    print(' ', end=' ')
-                col += 1
-            print()
-            row += 1
-    elif triangle_number == 2:
-        print("Правый треугольник (2):")
-        row = 0
-        while row < n:
-            col = 0
-            while col < n:
-                if row <= col and row + col >= n - 1:
-                    print('*', end=' ')
-                else:
-                    print(' ', end=' ')
-                col += 1
-            print()
-            row += 1
-    elif triangle_number == 3:
-        print("Нижний треугольник (3):")
-        row = 0
-        while row < n:
-            col = 0
-            while col < n:
-                if row >= col and row + col >= n - 1:
-                    print('*', end=' ')
-                else:
-                    print(' ', end=' ')
-                col += 1
-            print()
-            row += 1
-    elif triangle_number == 4:
-        print("Левый треугольник (4):")
-        row = 0
-        while row < n:
-            col = 0
-            while col < n:
-                if row >= col and row + col <= n - 1:
-                    print('*', end=' ')
-                else:
-                    print(' ', end=' ')
-                col += 1
-            print()
-            row += 1
+
+    elif number_task == 2:
+        print("Ex_2:")
+
+    elif number_task == 3:
+        print("Ex_3:")
+
+    elif number_task == 4:
+        print("Ex_4:")
+
     else:
-        print("Неверный номер треугольника. Введите число от 1 до 4.")
+        print("Wrong number. Enter a number from 1 to 10.")
 except Exception as e:
     print(e)
     print('Error importing modules')
