90d97ee3b53e3ea27e8c0238fae054867592baa3
KOBCHUK <irravadi@gmail.com>
2023-12-28 21:22:57 +0200

    ex_11_done_2

--- main.py
+++ main.py
@@ -1,20 +1,59 @@
 try:
-    n = int(input("Enter number: "))
-    if n <= 0:
-        raise Exception('Number should be greater than 0')
-    if n % 2 == 0:
-        raise Exception('Number should be odd')
-    i = 0
-    while i < n:
-        j = 0
-        while j < n:
-            if (i <= j and i + j <= n - 1) or (i >= j and i + j >= n - 1):
-                print('*', end=' ')
-            else:
-                print(' ', end=' ')
-            j += 1
-        print()
-        i += 1
+    number_task = int(input("Enter the task: "))
+    if number_task == 1:
+        print("Ex_1:")
+        row = 0
+        while row < n:
+            col = 0
+            while col < n:
+                if row <= col and row + col <= n - 1:
+                    print('*', end=' ')
+                else:
+                    print(' ', end=' ')
+                col += 1
+            print()
+            row += 1
+    elif triangle_number == 2:
+        print("Правый треугольник (2):")
+        row = 0
+        while row < n:
+            col = 0
+            while col < n:
+                if row <= col and row + col >= n - 1:
+                    print('*', end=' ')
+                else:
+                    print(' ', end=' ')
+                col += 1
+            print()
+            row += 1
+    elif triangle_number == 3:
+        print("Нижний треугольник (3):")
+        row = 0
+        while row < n:
+            col = 0
+            while col < n:
+                if row >= col and row + col >= n - 1:
+                    print('*', end=' ')
+                else:
+                    print(' ', end=' ')
+                col += 1
+            print()
+            row += 1
+    elif triangle_number == 4:
+        print("Левый треугольник (4):")
+        row = 0
+        while row < n:
+            col = 0
+            while col < n:
+                if row >= col and row + col <= n - 1:
+                    print('*', end=' ')
+                else:
+                    print(' ', end=' ')
+                col += 1
+            print()
+            row += 1
+    else:
+        print("Неверный номер треугольника. Введите число от 1 до 4.")
 except Exception as e:
     print(e)
     print('Error importing modules')
